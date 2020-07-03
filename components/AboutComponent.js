04610c89ff8a68d03f65904acfec608297492cb1
Andrew Carrigan <carrigan223@gmail.com>
2020-07-03 02:35:39 -0700

    workshop1 complete

--- components/AboutComponent.js
+++ components/AboutComponent.js
@@ -1,25 +1,45 @@
-import React, { Component } from 'react';
-import { ScrollView } from 'react-native';
+import React, { Component } from "react";
+import { ScrollView, Text, FlatList } from "react-native";
+import { Card, ListItem } from "react-native-elements";
+import Mission from "./MissionComponent";
+import { PARTNERS } from "../Shared/partners";
 
 class About extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
+  constructor(props) {
+    super(props);
+    this.state = {
+      partners: PARTNERS,
+    };
+  }
 
-        };
-    }
+  static navigationOptions = {
+    title: "About Us",
+  };
 
-    static navigationOptions = {
-        title: "About Us",
+  render() {
+    const renderPartner = ({ item }) => {
+      return (
+        <ListItem
+          title={item.name}
+          subtitle={item.description}
+          leftAvatar={{ source: require("./images/bootstrap-logo.png") }}
+        />
+      );
     };
 
-    render() {
-        return (
-            <ScrollView>
-
-            </ScrollView>
-        );
-    }
+    return (
+      <ScrollView>
+        <Mission />
+        <Card title={"Community Partners"}>
+          <FlatList
+            data={this.state.partners}
+            renderItem={renderPartner}
+            keyExtractor={(item) => item.id.toString()}
+          />
+        </Card>
+      </ScrollView>
+    );
+  }
 }
 
 export default About;
