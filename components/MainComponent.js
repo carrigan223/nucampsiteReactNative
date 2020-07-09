b699e7050b19c7ab51941b4c4cf4612ebfdfb91d
Andrew Carrigan <carrigan223@gmail.com>
2020-07-09 10:27:50 -0700

    maybe my drawer is working

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -4,9 +4,11 @@ import Directory from "./DirectoryComponent";
 import CampsiteInfo from "./CampsiteInfoComponent";
 import About from "./AboutComponent";
 import Contact from "./ContactComponent";
-import { View, Platform, StyleSheet } from "react-native";
-import { createStackNavigator, createDrawerNavigator } from "react-navigation";
+import { View, Platform, StyleSheet, Text, ScrollView, Image } from 'react-native';
+import { createStackNavigator, createDrawerNavigator,
+  DrawerItems } from 'react-navigation';
 import { Icon } from "react-native-elements";
+import SafeAreaView from "react-native-safe-area-view";
 
 const ContactNavigator = createStackNavigator(
   {
@@ -120,65 +122,92 @@ const HomeNavigator = createStackNavigator(
 
 const MainNavigator = createDrawerNavigator(
   {
-    Home: { 
+    Home: {
       screen: HomeNavigator,
       navigationOptions: {
-        drawerIcon: ({tintColor}) => (
-          <Icon 
-            name='home'
-            type='font-awesome'
-            size={24}
-            color={tintColor}
-          />
-        )
-      } 
+        drawerIcon: ({ tintColor }) => (
+          <Icon name="home" type="font-awesome" size={24} color={tintColor} />
+        ),
+      },
     },
-    Directory: { 
+    Directory: {
       screen: DirectoryNavigator,
       navigationOptions: {
-        drawerIcon: ({tintColor}) => (
-          <Icon 
-            name='list'
-            type='font-awesome'
-            size={24}
-            color={tintColor}
-          />
-        )
-      } 
+        drawerIcon: ({ tintColor }) => (
+          <Icon name="list" type="font-awesome" size={24} color={tintColor} />
+        ),
+      },
     },
-    About: { 
+    About: {
       screen: AboutNavigator,
       navigationOptions: {
-        drawerLabel: 'About Us',
-        drawerIcon: ({tintColor}) => (
-          <Icon 
-            name='info-circle'
-            type='font-awesome'
+        drawerLabel: "About Us",
+        drawerIcon: ({ tintColor }) => (
+          <Icon
+            name="info-circle"
+            type="font-awesome"
             size={24}
             color={tintColor}
           />
-        )
-      } 
+        ),
+      },
     },
-    Contact: { 
+    Contact: {
       screen: ContactNavigator,
       navigationOptions: {
-        drawerLabel: 'Contact Us',
-        drawerIcon: ({tintColor}) => (
-          <Icon 
-            name='address-card'
-            type='font-awesome'
+        drawerLabel: "Contact Us",
+        drawerIcon: ({ tintColor }) => (
+          <Icon
+            name="address-card"
+            type="font-awesome"
             size={24}
             color={tintColor}
           />
-        )
-      } 
+        ),
+      },
     },
   },
   {
     drawerBackgroundColor: "#CEC8FF",
+    contentComponent: (props) => (
+      <ScrollView>
+          <SafeAreaView 
+              style={styles.container}
+              forceInset={{top: 'always', horizontal: 'never'}}>
+              <View style={styles.drawerHeader}>
+                  <View style={{flex: 1}}>
+                      <Image source={require('./images/logo.png')} style={styles.drawerImage} />
+                  </View>
+                  <View style={{flex: 2}}>
+                      <Text style={styles.drawerHeaderText}>NuCamp</Text>
+                  </View>
+              </View>
+              <DrawerItems {...props} />
+          </SafeAreaView>
+      </ScrollView>
+    ),
   }
 ); //this is our main navigator which is the drawer navigator we can pull from the left,each navigator has an icon
+//the contencComponent property is overriding the default drawer content with the component we built
+
+// const CustomDrawerContentComponent = (props) => (
+//   <ScrollView>
+//       <SafeAreaView 
+//           style={styles.container}
+//           forceInset={{top: 'always', horizontal: 'never'}}>
+//           <View style={styles.drawerHeader}>
+//               <View style={{flex: 1}}>
+//                   <Image source={require('./images/logo.png')} style={styles.drawerImage} />
+//               </View>
+//               <View style={{flex: 2}}>
+//                   <Text style={styles.drawerHeaderText}>NuCamp</Text>
+//               </View>
+//           </View>
+//           <DrawerItems {...props} />
+//       </SafeAreaView>
+//   </ScrollView>
+// );
+//try to figure out why the original way didnt work
 
 class Main extends Component {
   render() {
@@ -197,11 +226,31 @@ class Main extends Component {
 }
 
 const styles = StyleSheet.create({
-  stackIcon: {
-    marginLeft: 10,
-    color: "#fff",
-    fontSize: 24,
+  container: {
+      flex: 1,
   },
+  drawerHeader: {
+      backgroundColor: '#5637DD',
+      height: 140,
+      alignItems: 'center',
+      justifyContent: 'center',
+      flex: 1,
+      flexDirection: 'row'
+  },
+  drawerHeaderText: {
+      color: '#fff',
+      fontSize: 24,
+      fontWeight: 'bold'
+  },
+  drawerImage: {
+      margin: 10,
+      height: 60,
+      width: 60
+  },
+  stackIcon: {
+      marginLeft: 10,
+      color: '#fff',
+      fontSize: 24
+  }
 });
-
 export default Main;
